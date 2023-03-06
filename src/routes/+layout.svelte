ec8bf70a48d1ee70995ba356864c3276a8b944da
emrosas <emireles.rosas@gmail.com>
2023-03-06 12:17:00 -0600

    Global styling an routing test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '../app.css';
+</script>
+
+<ul>
+	<li><a href="/">About Us</a></li>
+	<li><a href="/projects">Projects</a></li>
+	<li><a href="/pricing">Pricing</a></li>
+</ul>
+
+<slot />
+
+<style>
+	ul {
+		list-style-type: none;
+		color: white;
+		display: flex;
+		gap: 2rem;
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+	}
+</style>
